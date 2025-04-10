c2dcab9cb74ae3aa2dda2a0d66dc72ed4cee061c
josephcheng <the_only_light@hotmail.com>
2025-04-11 05:42:09 +0800

    chore: set basename in BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/psle-bootcamp-bundles">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
