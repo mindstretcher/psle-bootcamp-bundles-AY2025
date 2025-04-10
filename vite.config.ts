9d16aed4d274d3003878ce798fd95801d3cfd2b3
josephcheng <the_only_light@hotmail.com>
2025-04-11 06:53:32 +0800

    chore: try base again

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: 'https://psle-bootcamp-bundles-ay2025.vercel.app/psle-bootcamp-bundles/',
   server: {
     host: "::",
     port: 8080,
